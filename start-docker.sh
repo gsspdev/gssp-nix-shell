f2dafb330b543c103acd01be0c8ff6e63a81c7e4
gsspdev <dev@gssp.io>
2023-09-04 22:49:03 -0700

    added a bunch more shell environments

--- start-docker.sh
+++ start-docker.sh
@@ -1,1 +1,9 @@
-docker run -it -v $(pwd)/gssp-shell:/gssp-shell nixos/nix
+docker run -it \
+-v $(pwd)/default:/default \
+-v $(pwd)/gssp-nvchad:/gssp-nvchad \
+-v $(pwd)/gssp-shell:/gssp-shell \
+-v $(pwd)/gssp-spacemacs:/gssp-spacemacs \
+-v $(pwd)/gssp-spacevim:/gssp-spacevim \
+-v $(pwd)/python-shell:/python-shell \
+-v $(pwd)/rust-shell:/rust-shell \
+nixos/nix
