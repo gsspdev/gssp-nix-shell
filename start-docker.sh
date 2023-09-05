230c7673110c2c5cd8fb601abab922227b7910f7
gsspdev <dev@gssp.io>
2023-09-04 23:47:44 -0700

    reduced the number of nix-shell environments in the shells directory

--- start-docker.sh
+++ start-docker.sh
@@ -1,8 +1,6 @@
 docker run -it \
 -v $(pwd)/shells/gssp-nvchad:/shells/gssp-nvchad \
--v $(pwd)/shells/gssp-shell:/shells/gssp-shell \
 -v $(pwd)/shells/gssp-spacemacs:/shells/gssp-spacemacs \
--v $(pwd)/shells/gssp-spacevim:/shells/gssp-spacevim \
 -v $(pwd)/shells/python-shell:/shells/python-shell \
 -v $(pwd)/shells/rust-shell:/shells/rust-shell \
 -v $(pwd)/default.nix:/default.nix \
