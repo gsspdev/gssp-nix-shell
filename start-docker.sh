c7f7715743661a0990a1d0193dea05f54fbc4dad
gsspdev <dev@gssp.io>
2023-09-04 23:11:44 -0700

    updated README

--- start-docker.sh
+++ start-docker.sh
@@ -1,8 +1,9 @@
 docker run -it \
--v $(pwd)/gssp-nvchad:/gssp-nvchad \
--v $(pwd)/gssp-shell:/gssp-shell \
--v $(pwd)/gssp-spacemacs:/gssp-spacemacs \
--v $(pwd)/gssp-spacevim:/gssp-spacevim \
--v $(pwd)/python-shell:/python-shell \
--v $(pwd)/rust-shell:/rust-shell \
+-v $(pwd)/shells/gssp-nvchad:/shells/gssp-nvchad \
+-v $(pwd)/shells/gssp-shell:/shells/gssp-shell \
+-v $(pwd)/shells/gssp-spacemacs:/shells/gssp-spacemacs \
+-v $(pwd)/shells/gssp-spacevim:/shells/gssp-spacevim \
+-v $(pwd)/shells/python-shell:/shells/python-shell \
+-v $(pwd)/shells/rust-shell:/shells/rust-shell \
+-v $(pwd)/default.nix:/default.nix \
 nixos/nix
