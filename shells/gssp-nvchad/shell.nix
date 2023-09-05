5692896d39200520a9c41db03197952976f69543
gsspdev <dev@gssp.io>
2023-09-05 13:57:07 +0000

    updated gssp-nvchad

--- shells/gssp-nvchad/shell.nix
+++ shells/gssp-nvchad/shell.nix
@@ -6,6 +6,12 @@ in
       pkgs.python3
       pkgs.curl
       pkgs.vim
+      pkgs.git
+      pkgs.nodejs
+      pkgs.neovim
+      pkgs.ripgrep
+      pkgs.wget
+      pkgs.alacritty
     ];
 
 #      env = {
@@ -18,6 +24,11 @@ in
     shellHook = ''
       export PS1="\u@\h >>> "
       touch touch-test.txt
+      curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+chmod u+x nvim.appimage
+./nvim.appimage
+      git clone https://github.com/NvChad/NvChad ~/.config/nvim
+      echo "i guess it's bi"
     '';
   }
   
